e4979069f1083e239ad7f5fbc3edf0aabdffc294
Radonier-X <radonier.x@gmail.com>
2026-01-20 21:14:38 +0530

    Features added

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "spoil-update-tick-interval",
+        setting_type = "startup",
+        default_value = 300,
+        minimum_value = 1,
+        maximum_value = 18000,
+        order = "a"
+    }
+})
